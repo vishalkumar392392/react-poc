d9cdf09c94a0889f1c043a9e95c73d09e37c5796
vishalkumar392392 <57318611+vishalkumar392392@users.noreply.github.com>
2021-04-12 11:06:08 +0530

    Added comment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Persons from './Persons/Persons'
 import SearchAppBar from './components/Navabar/Navbar'
 class App extends Component {
-  
+  // Added comment
   render() {
    
     return (
